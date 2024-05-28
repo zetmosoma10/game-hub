3793187d1a189a30ddd50e4f9f492ae9f79a29cf
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-05-28 21:51:55 +0200

    Create a generic data fetching hook

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,7 +3,6 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
-import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
@@ -11,20 +10,18 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <GameCardContainer>
-      <Card>
-        <Image src={getCroppedImageUrl(game.background_image)} />
-        <CardBody>
-          <Heading fontSize="2xl">{game.name}</Heading>
-          <HStack justifyContent="space-between">
-            <PlatformIconList
-              platform={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-        </CardBody>
-      </Card>
-    </GameCardContainer>
+    <Card>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platform={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
+    </Card>
   );
 };
 
